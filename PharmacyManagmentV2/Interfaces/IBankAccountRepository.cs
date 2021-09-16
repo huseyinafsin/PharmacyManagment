40819dc82414fc6694f9af7d7523e1902fc0d766
huseyinafsin <huseyinafsin4@gmail.com>
2021-09-16 11:55:38 +0300

    Bank changes

---
+++ PharmacyManagmentV2/Interfaces/IBankAccountRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PharmacyManagmentV2.Data;
+using PharmacyManagmentV2.Models;
+
+namespace PharmacyManagmentV2.Interfaces
+{
+    public interface IBankAccountRepository : IGenericRepository<BankAccount>
+    {
+        void Deducate(BankAccount account, string accountType, int quantity);
+        BankAccount GetBankAccount();
+    }
+}
---
+++ PharmacyManagmentV2/Repositories/BankAccountRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PharmacyManagmentV2.Contexts;
+using PharmacyManagmentV2.Data;
+using PharmacyManagmentV2.Interfaces;
+using PharmacyManagmentV2.Models;
+
+namespace PharmacyManagmentV2.Repositories
+{
+    public class BankAccountRepository : GenericRepository<BankAccount>, IBankAccountRepository
+    {
+        private readonly AppDBContext _context;
+        public BankAccountRepository(AppDBContext context) : base(context)
+        {
+            _context = context;
+        }
+
+
+
+        public void Deducate(BankAccount account, string accountType, int quantity)
+        {
+            if (accountType.ToLowerInvariant()=="credit")
+            {
+                
+            }
+            else if (accountType.ToLowerInvariant()=="balance")
+            {
+
+            }
+            else
+            {
+                throw new Exception("Wrong account type");
+            }
+        }
+
+        public BankAccount GetBankAccount()
+        {
+            BankAccount bankAccount = new BankAccount();
+            _context.BankAccounts.Add(bankAccount);
+            return bankAccount;
+        }
+
+    }
+}
